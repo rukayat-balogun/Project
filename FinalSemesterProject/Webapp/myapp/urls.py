5f1e84e2f552456aa43cf0d7d8a21ab765de00b1
rukayat-balogun <rukayat.balogun111@gmail.com>
2024-09-18 22:02:21 +0100

    updated

--- FinalSemesterProject/Webapp/myapp/urls.py
+++ FinalSemesterProject/Webapp/myapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.home, name='home'),
+  path('fitness', views.home, name='home'),
   path('calculate_bmi/', views.calculate_bmi, name='calculate_bmi'),
   path('thank-you/', views.thank_you, name='thank_you'),
   path('feedback/', views.feedback_view, name='feedback'),
